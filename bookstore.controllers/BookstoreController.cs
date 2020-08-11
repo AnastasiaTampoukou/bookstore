56aceb812fe9778497524ee44894af6deb7992f7
Moudilos Ioannis <moudilos.ioannis@nbg.gr>
2020-08-11 16:11:45 +0300

    - injects logger to Controller instead of using Logger.Current

--- bookstore.controllers/BookstoreController.cs
+++ bookstore.controllers/BookstoreController.cs
@@ -2,32 +2,24 @@
 using System;
 using System.Web.Http;
 
-
 namespace bookstore.controllers
 {
-    
     public class BookstoreController : ApiController
     {
-        private static readonly string _logSourceName = "BookstoreApiSource";
-        private static readonly ILogger _logger = new Logger(_logSourceName);
+        private readonly ILogger _logger;
 
-        public BookstoreController() { }
+        public BookstoreController(ILogger logger)
+        {
+            _logger = logger;
+        }
 
         [HttpGet]
         public string Test()
         {
             DateTime now = DateTime.Now;
-            Logger.Current.Info($"Hello the current DateTime is: {now}");
-               
+            _logger.Info($"Hello the current DateTime is: {now}");
+
             return $"Hello the current DateTime is: {now}";
         }
-
-        private static void RegisterBookstore()
-        {
-            
-        }
-
-        
-
     }
 }
