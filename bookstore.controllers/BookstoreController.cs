9432fff144830865f267e9de9811914b182f66d6
Moudilos Ioannis <moudilos.ioannis@nbg.gr>
2020-08-12 13:38:52 +0300

    - BookstoreController: renames IBookstoreService field

--- bookstore.controllers/BookstoreController.cs
+++ bookstore.controllers/BookstoreController.cs
@@ -9,19 +9,19 @@ namespace bookstore.controllers
     public class BookstoreController : ApiController
     {
         private readonly ILogger _logger;
-        private readonly IBookstoreService _book;
+        private readonly IBookstoreService _bookstoreService;
 
-        public BookstoreController(ILogger logger, IBookstoreService book)
+        public BookstoreController(ILogger logger, IBookstoreService bookstoreService)
         {
             _logger = logger;
-            _book = book;
+            _bookstoreService = bookstoreService;
         }
 
 
         [HttpGet]
         public string Test()
         {
-            _logger.Info($"Test endpoint called on : {_book.Test()}");
+            _logger.Info($"Test endpoint called on : {_bookstoreService.Test()}");
             return "Hello from Bookstore API";
         }
     }
